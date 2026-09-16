7791422c2ce0b5efb193e68d8ce2f2e8948a9fee
coq-j01 <coqls0311@gmail.com>
2026-09-16 20:10:18 +0900

    로그인 시 확인을 위해 CustomUserDetailsService 추가

---
+++ src/main/java/com/studymate/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.studymate.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.studymate.domain.Member;
+import com.studymate.mapper.MemberMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+	private final MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberMapper.findByEmail(username);
+		if(member == null) {
+			throw new UsernameNotFoundException("존재하지 않는 회원입니다.");
+		}
+		return User.builder()
+				.username(member.getEmail())
+				.password(member.getPassword())
+				.roles("USER")
+				.build();
+	}
+
+}
