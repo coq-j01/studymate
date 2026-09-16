cf14b41ab7bb61647375e6bbf5cbc9e0948e0b19
coq-j01 <coqls0311@gmail.com>
2026-09-16 18:42:56 +0900

    회원가입 및 로그인용 MyBatis Mapper 추가

---
+++ src/main/java/com/studymate/mapper/MemberMapper.java
@@ -0,0 +1,19 @@
+package com.studymate.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.studymate.domain.Member;
+import com.studymate.dto.JoinDTO;
+
+@Mapper
+public interface MemberMapper {
+	
+	//이메일로 회원조회
+	Member findByEmail(String email);
+	
+	//닉네임 중복조회
+	Member findByNickname(String nickname);
+	
+	//회원 추가, 성공여부 반환(0:실패, 1:성공)
+	int insertMember(JoinDTO joinDTO);
+}
