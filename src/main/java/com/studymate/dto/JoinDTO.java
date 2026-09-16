4524954ce6d4e040bd2dfe4ba832e91efcbeae23
coq-j01 <coqls0311@gmail.com>
2026-09-16 20:07:55 +0900

    valid 추가

--- src/main/java/com/studymate/dto/JoinDTO.java
+++ src/main/java/com/studymate/dto/JoinDTO.java
@@ -1,13 +1,32 @@
 package com.studymate.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class JoinDTO {
-	private String email;
-	private String password;
-	private String passwordConfirm;
-	private String name;
-	private String nickname;
-	private String gender;
+	 	@NotBlank
+	    @Email
+	    private String email;
+
+	    @NotBlank
+	    @Size(min = 8)
+	    private String password;
+
+	    @NotBlank
+	    private String passwordConfirm;
+
+	    @NotBlank
+	    private String name;
+
+	    @NotBlank
+	    private String nickname;
+
+	    @NotBlank
+	    @Pattern(regexp = "F|M")
+	    private String gender;
 }
