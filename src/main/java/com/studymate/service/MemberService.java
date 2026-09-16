7640f3a5daa6d69e09698890488fc68fcef4643a
coq-j01 <coqls0311@gmail.com>
2026-09-16 20:08:20 +0900

    join함수 타입 boolean으로 변경

--- src/main/java/com/studymate/service/MemberService.java
+++ src/main/java/com/studymate/service/MemberService.java
@@ -3,7 +3,7 @@ package com.studymate.service;
 import com.studymate.dto.JoinDTO;
 
 public interface MemberService {
-	void join(JoinDTO joindto);
+	boolean join(JoinDTO joindto);
 	boolean isEmailDuplicate(String email);
 	boolean isNicknameDuplicate(String nickname);
 }
