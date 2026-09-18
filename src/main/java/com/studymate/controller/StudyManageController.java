7651c71e7ff7ef2f1544e2f5b883beb1bff31143
coq-j01 <coqls0311@gmail.com>
2026-09-18 17:22:00 +0900

    modelattribute 추가 및 강퇴 post 추가

--- src/main/java/com/studymate/controller/StudyManageController.java
+++ src/main/java/com/studymate/controller/StudyManageController.java
@@ -1,18 +1,19 @@
 package com.studymate.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.studymate.mapper.StudyJoinRequestMapper;
-import com.studymate.mapper.StudyMapper;
 import com.studymate.security.CustomUserDetails;
 import com.studymate.service.StudyMemberService;
+import com.studymate.service.StudyService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,74 +21,88 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/study/{studyId}/manage")
 public class StudyManageController {
-	private final StudyMapper studyMapper;
-	private final StudyMemberService memberService;
-	
-	private boolean isLeader(int studyId,int loginMemberId) {
-		return studyMapper.isStudyLeader(studyId,loginMemberId);
+	private final StudyMemberService studyMemberService;
+	private final StudyService studyService;
+
+	@ModelAttribute
+	public void addStudyId(@PathVariable int studyId, Model model) {
+
+		model.addAttribute("studyId", studyId);
 	}
-	//신청서 페이지
+
+	private boolean isLeader(int studyId, int loginMemberId) {
+		return studyService.isLeader(studyId, loginMemberId);
+	}
+
+	// 신청서 페이지
 	@GetMapping("/requests")
-    public String requests(@PathVariable int studyId,
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            Model model) {
-		if(!isLeader(studyId,userDetails.getMemberId())) {
+	public String requests(@PathVariable int studyId, @AuthenticationPrincipal CustomUserDetails userDetails,
+			Model model) {
+		if (!isLeader(studyId, userDetails.getMemberId())) {
 			return "redirect:/study/" + studyId + "/home";
 		}
-		model.addAttribute("requestList",memberService.getJoinRequestList(studyId));
+		model.addAttribute("joinRequestList", studyMemberService.getJoinRequestList(studyId));
 		return "study/manage/requests";
-    }
-	//신청서 승인
+	}
+
+	// 신청서 승인
 	@PostMapping("/requests/{memberId}/approve")
-	public ResponseEntity<String> approveJoinRequest(
-	        @PathVariable int studyId,
-	        @PathVariable int memberId,
-	        @AuthenticationPrincipal CustomUserDetails userDetails){
+	public ResponseEntity<String> approveJoinRequest(@PathVariable int studyId, @PathVariable int memberId,
+			@AuthenticationPrincipal CustomUserDetails userDetails) {
 		try {
 
-			memberService.approveJoinRequest(
-	                studyId,
-	                memberId,
-	                userDetails.getMemberId()
-	        );
+			studyMemberService.approveJoinRequest(studyId, memberId, userDetails.getMemberId());
 
-	        return ResponseEntity.ok("가입 신청을 수락했습니다.");
+			return ResponseEntity.ok("가입 신청을 수락했습니다.");
 
-	    } catch (IllegalStateException e) {
+		} catch (IllegalStateException e) {
 
-	        return ResponseEntity
-	                .badRequest()
-	                .body(e.getMessage());
-	    }
-	}
-	
-	//신청서 거절
-		@PostMapping("/requests/{memberId}/reject")
-		public ResponseEntity<String> rejectJoinRequest(
-		        @PathVariable int studyId,
-		        @PathVariable int memberId,
-		        @AuthenticationPrincipal CustomUserDetails userDetails){
-			try {
-
-				memberService.rejectJoinRequest(
-		                studyId,
-		                memberId,
-		                userDetails.getMemberId()
-		        );
-
-		        return ResponseEntity.ok("가입 신청을 거절했습니다.");
-
-		    } catch (IllegalStateException e) {
-
-		        return ResponseEntity
-		                .badRequest()
-		                .body(e.getMessage());
-		    }
+			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+
+	// 신청서 거절
+	@PostMapping("/requests/{memberId}/reject")
+	public ResponseEntity<String> rejectJoinRequest(@PathVariable int studyId, @PathVariable int memberId,
+			@AuthenticationPrincipal CustomUserDetails userDetails) {
+		try {
+
+			studyMemberService.rejectJoinRequest(studyId, memberId, userDetails.getMemberId());
+
+			return ResponseEntity.ok("가입 신청을 거절했습니다.");
+
+		} catch (IllegalStateException e) {
+
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	// 강퇴하기
+	@PostMapping("/members/{memberId}/kick")
+	public ResponseEntity<String> kickMember(@PathVariable int studyId, @PathVariable int memberId,
+			@AuthenticationPrincipal CustomUserDetails userDetails) {
+		if (!isLeader(studyId, userDetails.getMemberId())) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("스터디장만 강퇴할 수 있습니다.");
+		}
+		studyMemberService.inactiveMember(studyId, memberId);
+
+		return ResponseEntity.ok("스터디원을 강퇴했습니다.");
+
+	}
+
+	@GetMapping("/members")
+	public String getMembers(@PathVariable int studyId, @AuthenticationPrincipal CustomUserDetails userDetails,
+			Model model) {
+		model.addAttribute("memberList", studyMemberService.getStudyMemberList(studyId));
+		if (!isLeader(studyId, userDetails.getMemberId())) {
+			return "redirect:/study/" + studyId + "/home";
+		}
+		model.addAttribute("memberList", studyMemberService.getStudyMemberList(studyId));
+
+		return "study/manage/members";
+	}
 
 	/*
-	 * @GetMapping("/members") public String members(...) { ... }
-	 * 
 	 * @GetMapping("/settings") public String settings(...) { ... }
 	 */
 }
